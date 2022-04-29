643fe878da2a6c570ab42738fdfb9a5fe76ca5a7
Luuzaall <luuzaall@gmail.com>
2022-04-29 09:20:54 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 // Import stylesheets
-import './style.css';
+// import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let saludo: string = 'Hola como sta ?s';
+console.log(saludo);
+
+/* los achivos de configuración son de tipo JSON 
+
+"module": "commonjs", se tendrá una clase para definir. Es un estándar para reutilizar y simplificar código.
+"outDir": "out", Donde quiero que esté el código transpilado.
+"sourceMap": true, para indicar si se quiere un archivo que mapee.
+*/
